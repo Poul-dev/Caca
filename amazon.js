90e67ff65ee3682e8d2c1c35369e0f7e0c997906
Poul-dev <69421779+Poul-dev@users.noreply.github.com>
2021-05-16 10:30:36 +0200

    Delete amazon.js

--- amazon.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-
-bot.on('ready', function() {
-    console.log('ready')
-    bot.user.setActivity('revendre des cacas').catch(console.error)
-})
-
-bot.login('ODQzNDAwNDE1NDg3NTkwNDMw.YKDT2A.mwZ_bOxw9n-zqobReIGGurlAxV4')
